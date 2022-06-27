7173ab0a73104ab512826303a204563ec62f231f
Maria <marymp@gmail.com>
2022-06-28 00:46:16 +0200

    first commit

---
+++ src/main/java/com/ironhack/midtermmariamoyano/MidtermMariamoyanoApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.midtermmariamoyano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermMariamoyanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermMariamoyanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/midtermmariamoyano/MidtermMariamoyanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.midtermmariamoyano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermMariamoyanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
