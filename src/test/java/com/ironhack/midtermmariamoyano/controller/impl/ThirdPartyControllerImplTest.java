ce852cdda11a2db9de7c07998ca9265395687c99
Maria <marymp@gmail.com>
2022-07-03 22:43:04 +0200

    .

---
+++ src/test/java/com/ironhack/midtermmariamoyano/controller/impl/ThirdPartyControllerImplTest.java
@@ -0,0 +1,38 @@
+package com.ironhack.midtermmariamoyano.controller.impl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ironhack.midtermmariamoyano.classes.Address;
+import com.ironhack.midtermmariamoyano.models.AccountHolder;
+import com.ironhack.midtermmariamoyano.models.Admin;
+import com.ironhack.midtermmariamoyano.models.Role;
+import com.ironhack.midtermmariamoyano.models.User;
+import com.ironhack.midtermmariamoyano.repository.CreditCardRepository;
+import com.ironhack.midtermmariamoyano.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc // new annotation
+public class ThirdPartyControllerImplTest {
+}
